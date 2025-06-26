9a31508b37a5fd60e0188a433d9e34e2531b4bed
siongmi <tldmseeo@naver.com>
2025-06-26 10:05:18 +0900

    간트 고쳐야함

--- src/app/user/menu/production-gantt/page.js
+++ src/app/user/menu/production-gantt/page.js
@@ -5,15 +5,12 @@ import { GanttComponent, Inject, Selection } from "@syncfusion/ej2-react-gantt";
 import { registerLicense } from "@syncfusion/ej2-base";
 import "@syncfusion/ej2-react-gantt/styles/material.css";
 
-// 라이선스 등록
 registerLicense(
   "Ngo9BigBOggjHTQxAR8/V1NNaF1cWWhOYVFpR2Nbek5zflZCallZVAciSV9jS3tTcEdmWXxddHFdR2ZdUk90Vg=="
 );
 
-// 문자열 → 날짜 변환
 const parseDate = (str) => new Date(str);
 
-// 간트 차트 시작/종료 시간 설정
 const startView = new Date("2025-06-23T08:45:00");
 const endView = new Date("2025-06-23T12:00:00");
 
@@ -73,9 +70,9 @@ export default function ProductionGanttPage() {
           bottomTier: { unit: "Minutes", count: 5, format: "mm" },
         }}
         labelSettings={{ leftLabel: "TaskName" }}
-        treeColumnIndex={1} // TaskName이 컬럼 두 번째에 표시됨
+        treeColumnIndex={1}
         gridLines="None"
-        splitterSettings={{ position: "30%" }} // 좌측 ID+Name 30%, 우측 차트 70%
+        splitterSettings={{ position: "30%" }}
         columns={[
           {
             field: "TaskID",
