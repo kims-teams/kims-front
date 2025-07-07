dfad7ec27eea4262b746ffdceb0de82b2f18ecd8
siongmi <tldmseeo@naver.com>
2025-07-07 15:19:14 +0900

    찐막

--- src/app/user/menu/result/page.js
+++ src/app/user/menu/result/page.js
@@ -4,10 +4,5 @@ import useAuthRedirect from "../../../../hooks/useAuthRedirect";
 
 export default function ResultPage() {
   useAuthRedirect();
-
-  return (
-    <div>
-      <h2>실행 결과</h2>
-    </div>
-  );
+  
 }
--- src/app/user/menu/scenario/page.js
+++ src/app/user/menu/scenario/page.js
@@ -5,12 +5,4 @@ import useAuthRedirect from "../../../../hooks/useAuthRedirect";
 
 export default function ScenarioPage() {
   useAuthRedirect();
-
-  return (
-    <Box sx={{ p: 3 }}>
-      <Typography variant="h5" gutterBottom>
-        시나리오 관리
-      </Typography>
-    </Box>
-  );
 }
